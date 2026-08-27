2877249c871be466d7ca4a9cc04ca496b3bc2483
Gabriel-Goncalves <ggoncalves.tech@outlook.com>
2026-08-27 15:16:06 -0300

    Adding fontsource for Ubuntu and navigation for route to login page

--- app/client/src/containers/app/App.jsx
+++ app/client/src/containers/app/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import { Login } from "../login/login";
-import {BrowserRouter, Routes, Route, redirect} from "react-router-dom"
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" action={redirect("/login")} />
+          <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
